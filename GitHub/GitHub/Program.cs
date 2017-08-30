31f901e417e2ad1d95819babe00a862a99d02e71
GudrunTricentis <31444953+GudrunTricentis@users.noreply.github.com>
2017-08-30 09:51:40 +0200

    Commit

---
+++ GitHub/GitHub/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Octokit;
+
+namespace GitHub
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var client = new GitHubClient(new ProductHeaderValue("TryOut_GitHub1"));
+
+            var basicAuth = new Credentials("GudrunTricentis", "GitHub11#");
+
+            client.Credentials = basicAuth;
+
+            IRepositoryBranchesClient branches = client.Repository.Branch;
+
+        }
+    }
+}
